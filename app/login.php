b9f937a2d2644593c53c93df10f5d31698bc2552
ryo04hub <rsaka1004@gmail.com>
2017-06-09 12:00:36 +0900

    コメントの追加・修正

--- app/login.php
+++ app/login.php
@@ -4,28 +4,51 @@ require_once ('../util/defineUtil.php');
 require_once("../util/dbaccessUtil.php");
 require_once("../util/scriptUtil.php");
 
+/* -------------------------------------------------------------------
+
+  POSTでログインフォームからの情報を受け取る処理
+
+------------------------------------------------------------------- */
+
 !isset($_POST['username']) ? $_SESSION['username'] = 'Guestユーザー' : $_SESSION['username'] = $_POST['username'];
 
 !isset($_POST['password']) ? $password = '' : $password = $_POST['password'];
 
-!isset($_POST['username']) ? $userdatas = array() : $userdatas = search_users($_POST['username']);
+!isset($_POST['username']) ? $userdatas = array() : $userdatas = search_users($_POST['username']); //未入力時に配列として初期化しているのは、のちにforeachで展開する際のエラーを回避するため
 
 
-//初期化
-$loginMessage = '';
+/* -------------------------------------------------------------------
+
+  ログイン時、エラーメッセージを表示するための処理
+
+------------------------------------------------------------------- */
+
+$loginMessage = ''; //初期化
 
 //後のパスワード判定に使うために配列を展開
 if (!empty($userdatas)) {
   foreach ($userdatas as $key => $value) {
     $userdata =  $value;
   }
-
 } else {
-    $userdata['deleteFlg'] = ""; //後のerror回避のため
+    $userdata['deleteFlg'] = ""; //後のerror回避のため。$userdata['deleteFlg']になにも入っていないとログイン判定時にエラーが出てしまう。
 }
 
-// search_usersで検索した$userdatasは、一致するユーザーが見つからないときにarray(0){}を返す
-// そして、emptyを使って、そのnullを検知する
+/**
+* ログイン成功時の判定の解説
+*
+* !empty($userdatas)
+*   $userdatasはsearch_usersというユーザー定義関数で検索して取得しているが、これは一致するユーザーが見つからないときにarray(0){}を返す。
+*   array(0){}はnullということなので、emptyを使ってそのnullを検知する
+*
+* array_search($password, $userdata) !== false
+*   $userdataの中に、フォームから受け取ったパスワードが存在しているかをチェックしている。見つからなかった場合にはfalseを返す。
+*   なお、$userdataは上記のforeach文で取得している。
+*
+* $userdata['deleteFlg'] == 0
+*   ユーザーが削除されているかどうかの判定
+*/
+
 if (!empty($userdatas) && array_search($password, $userdata) !== false && $userdata['deleteFlg'] == 0) {
    $loginMessage =  "ログインに成功しました。<br>" ."<a href='".ROOT_URL."'>トップページへ</a>";
 
